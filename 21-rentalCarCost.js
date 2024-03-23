1c0ce492797e7d902affb04691b7125ef7d8dcfa
Fernanda Mahesa <esa@ip-192-168-1-2.ap-southeast-1.compute.internal>
2024-03-23 23:47:13 +0700

    day 21/366

---
+++ 21-rentalCarCost.js
@@ -0,0 +1,17 @@
+//WPU Coding Challenge 2024
+//19/366
+// https://www.codewars.com/kata/568d0dd208ee69389d000016
+
+// function rentalCarCost(d) {
+//   let disc = 0;
+//   if (d >= 7) {
+//     disc = 50;
+//   } else if (d >= 3) {
+//     disc = 20;
+//   }
+//   return d * 40 - disc;
+// }
+
+const rentalCarCost = (d) => d * 40 - (d >= 7 ? 50 : d >= 3 ? 20 : 0);
+
+console.log(rentalCarCost(10));
